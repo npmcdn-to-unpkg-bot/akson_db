100dc192ea5d8733267a2c4d32f7fcccf64aa95f
wesolutki <wesolutki@wp.pl>
2016-04-14 00:47:31 +0200

    removed person

--- person/__init__.py
+++
@@ -1,7 +0,0 @@
-from common.apps import default_app_config_string, common_app_config
-default_app_config = default_app_config_string(__file__)
-
-
-@common_app_config(__file__)
-class AppConfig(object):
-    pass
--- person/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from common.admintest import adminviews_test
-
-
-class PersonTest(TestCase):
-    def test_admin_views(self):
-        adminviews_test(self)
