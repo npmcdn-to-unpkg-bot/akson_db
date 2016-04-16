e73805b2cb28001f393f850e74bce8636045fffe
wesolutki <wesolutki@wp.pl>
2016-04-16 14:15:24 +0200

    massage_reflexology_card

---
+++ massage_reflexology_card/admin.py
@@ -0,0 +1,24 @@
+from common.admin import AksonCardAdmin, admin
+from common.feature import FeatureCardAdmin
+from common.localization import txt
+from massage_reflexology_card.models import *
+
+
+class MassageReflexologyCardCardAdmin(AksonCardAdmin):
+    fieldsets = (
+        (txt('General'),
+            {'fields': (
+                ('therapist', 'patient', 'date',),
+            )}),
+        (txt('Info'),
+            {'fields': (('massage_reflexology_treatment', 'massage_reflexology_area'),
+            )}),
+        (txt('Additionals'),
+            {'fields': (('additional_notes'),
+            )}),
+            )
+    list_display = ('patient', 'date',)
+
+admin.site.register(MassageReflexologyCard, MassageReflexologyCardCardAdmin)
+admin.site.register(MassageReflexologyTreatment, FeatureCardAdmin)
+admin.site.register(MassageReflexologyArea, FeatureCardAdmin)
