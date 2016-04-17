ba00f283f453d3d3671ce0b01f16563dec7deba3
wesolutki <wesolutki@wp.pl>
2016-04-17 17:13:57 +0200

    added inlines to patient

--- patient/admin.py
+++ patient/admin.py
@@ -3,8 +3,16 @@ from copy import deepcopy
 from common.admin import AksonBaseAdmin, admin
 from common.localization import txt, verbose_names_inline
 from patient.models import Patient
-from neurorehabilitation_card.models import NeurorehabilitationCard
-from neurorehabilitation_card.admin import NeurorehabilitationCardCardAdmin
+from gait_reeducation_card.admin import GaitReeducationCard, GaitReeducationCardCardAdmin
+from massage_reflexology_card.admin import MassageReflexologyCard, MassageReflexologyCardCardAdmin
+from metric_card.admin import MetricCard, MetricCardCardAdmin
+from neurorehabilitation_card.admin import NeurorehabilitationCard, NeurorehabilitationCardCardAdmin
+from physiotherapy_card.admin import PhysiotherapyCard, PhysiotherapyCardCardAdmin
+from time_spread.admin import TimeSpread, TimeSpreadCardAdmin
+from track_gait_training.admin import TrackGaitTraining, TrackGaitTrainingCardAdmin
+from treatment_record.admin import TreatmentRecord, TreatmentRecordCardAdmin
+from upper_limb_study.admin import UpperLimbStudy, UpperLimbStudyCardAdmin
+from upper_limb_therapy.admin import UpperLimbTherapy, UpperLimbTherapyCardAdmin
 
 
 class PatientInlineBase(object):
@@ -34,25 +42,83 @@ class PatientInline(PatientInlineBase, admin.TabularInline):
     pass
 
 
+class PatientStackedInline(PatientInlineBase, admin.StackedInline):
+    pass
+
+
+@verbose_names_inline
+class GaitReeducationCardInline(PatientInline):
+    model = GaitReeducationCard
+    fieldsets = deepcopy(GaitReeducationCardCardAdmin.fieldsets)
+
+
+@verbose_names_inline
+class TrackGaitTrainingInline(PatientInline):
+    model = TrackGaitTraining
+    fieldsets = deepcopy(TrackGaitTrainingCardAdmin.fieldsets)
+
+
 @verbose_names_inline
 class NeurorehabilitationCardInline(PatientInline):
     model = NeurorehabilitationCard
     fieldsets = deepcopy(NeurorehabilitationCardCardAdmin.fieldsets)
 
 
+@verbose_names_inline
+class PhysiotherapyCardInline(PatientInline):
+    model = PhysiotherapyCard
+    fieldsets = deepcopy(PhysiotherapyCardCardAdmin.fieldsets)
+
+
+@verbose_names_inline
+class MetricCardInline(PatientStackedInline):
+    model = MetricCard
+    fieldsets = deepcopy(MetricCardCardAdmin.fieldsets)
+
+
+@verbose_names_inline
+class TreatmentRecordInline(PatientInline):
+    model = TreatmentRecord
+    fieldsets = deepcopy(TreatmentRecordCardAdmin.fieldsets)
+
+
+@verbose_names_inline
+class TimeSpreadInline(PatientInline):
+    model = TimeSpread
+    fieldsets = deepcopy(TimeSpreadCardAdmin.fieldsets)
+
+
+@verbose_names_inline
+class UpperLimbStudyInline(PatientStackedInline):
+    model = UpperLimbStudy
+    fieldsets = deepcopy(UpperLimbStudyCardAdmin.fieldsets)
+
+
+@verbose_names_inline
+class MassageReflexologyCardInline(PatientInline):
+    model = MassageReflexologyCard
+    fieldsets = deepcopy(MassageReflexologyCardCardAdmin.fieldsets)
+
+
+@verbose_names_inline
+class UpperLimbTherapyInline(PatientInline):
+    model = UpperLimbTherapy
+    fieldsets = deepcopy(UpperLimbTherapyCardAdmin.fieldsets)
+
+
 class PatientCardAdmin(AksonBaseAdmin):
     model = Patient
     inlines = [
-        #     TimeSpreadInline,
-        #     GaitReeducationCardInline,
-        #     TrackGaitTrainingInline,
+        TimeSpreadInline,
+        GaitReeducationCardInline,
+        TrackGaitTrainingInline,
         NeurorehabilitationCardInline,
-        #     PhysiotherapyCardInline,
-        #     MetricCardInline,
-        #     UpperLimbStudyInline,
-        #     UpperLimbTherapyInline,
-        #     MassageReflexologyCardInline,
-        #     TreatmentRecordInline,
+        PhysiotherapyCardInline,
+        MetricCardInline,
+        UpperLimbStudyInline,
+        UpperLimbTherapyInline,
+        MassageReflexologyCardInline,
+        TreatmentRecordInline,
         #            # AsiaCardInline,
     ]
     fieldsets = (
