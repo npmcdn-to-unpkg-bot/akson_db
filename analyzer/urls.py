3fc5568c91cdbc8514808288d526fc0abc94e604
wesolutki <wesolutki@wp.pl>
2016-08-21 21:45:07 +0200

    added signatures REST to analyzer

--- analyzer/urls.py
+++ analyzer/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^patients/$', views.get_patients, name='get_patients')
+    url(r'^patients/$', views.get_patients, name='get_patients'),
+    url(r'^signatures/$', views.get_signatures, name='get_signatures'),
 ]
--- analyzer/views.py
+++ analyzer/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.http import JsonResponse
 
 from patient.models import Patient
+from neurorehabilitation_card.models import ExcerciseSignature
 
 
 def check_superuser(user):
@@ -20,3 +20,10 @@ def index(request):
 @user_passes_test(check_superuser)
 def get_patients(request):
     return JsonResponse({'patients': list(Patient.objects.values_list('id', 'first_name', 'last_name'))})
+
+
+@login_required(login_url='/login/')
+@user_passes_test(check_superuser)
+def get_signatures(request):
+    return JsonResponse({'signatures': list(ExcerciseSignature.objects.values_list('id', 'name', 'description'))})
+
