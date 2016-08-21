2503bf7089c4ab7dac093663046a1ccd2851d5c4
Janusz Woźniak <janusz.wozniak@collibra.com>
2016-08-22 00:13:54 +0200

    Added support for jquery

--- analyzer/src/components/Hello.tsx
+++ analyzer/src/components/Hello.tsx
@@ -1,10 +1,27 @@
 import * as React from "react";
+import * as $ from "jquery"
 
-export interface HelloProps { compiler: string; framework: string; }
+export interface HelloProps {
+  compiler: string;
+    framework: string; }
+
 
 export class Hello extends React.Component<HelloProps, {}> {
+
+    componentWillMount = () => {
+      $.get("./analyzer/api/patients", (result) => {
+          console.log(result);
+      })
+    };
+
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return (
+          <select>
+              <option value="volvo">Volvo</option>
+              <option value="saab">Saab</option>
+              <option value="vw">VW</option>
+              <option value="audi" selected>Audi</option>
+          </select>
+        );
     }
 }
-
