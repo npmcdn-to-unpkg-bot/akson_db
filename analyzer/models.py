87e13f7f1caf11d107b1f55cc17501b837e7d8e6
wesolutki <wesolutki@wp.pl>
2016-08-22 00:40:12 +0200

    simple model in analyzer

---
+++ analyzer/models.py
@@ -0,0 +1,21 @@
+from common.models import *
+from common.localization import verbose_names
+from patient.models import Patient
+from datetime import datetime
+
+
+@verbose_names
+class NeurorehabilitationChart(models.Model):
+    patient = models.ForeignKey(Patient)
+    name = models.CharField(max_length=200)
+    start_date = models.DateField(default=datetime.now)
+    end_date = models.DateField(default=datetime.now, blank=True)
+    excercise_signatures = models.ManyToManyField('ExcerciseSignature')
+
+    additional_notes = AdditionalNotesField(blank=True, null=True)
+
+    class Meta:
+        ordering = ('patient', 'date',)
+
+    def __str__(self):
+        return "{0} - {1} - {2}".format(self.patient, self.name, self.start_date, self.end_date)
