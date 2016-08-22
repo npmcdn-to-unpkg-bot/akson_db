3449780fcc850163cc7fcf114c001ab0f071a9d5
Janusz Woźniak <janusz.wozniak@collibra.com>
2016-08-22 02:39:20 +0200

    Added onClick functionality, typings for google-chart and react-bootstrap

--- analyzer/src/components/Hello.tsx
+++ analyzer/src/components/Hello.tsx
@@ -1,27 +1,81 @@
 import * as React from "react";
 import * as $ from "jquery"
 
+import PatientCharts from "./PatientCharts"
+
 export interface HelloProps {
   compiler: string;
-    framework: string; }
+    framework: string;
+}
+
+const enum View {
+  MAIN,
+  PATIENT,
+  CHART
+};
+
+export interface PatientsState {
+  patients: any[];
+  view: View;
+  currentPatientId?: number;
+}
 
 
-export class Hello extends React.Component<HelloProps, {}> {
+export class Hello extends React.Component<HelloProps, PatientsState> {
+  public state = {
+    patients: [{}],
+    view: View.MAIN
+  };
 
-    componentWillMount = () => {
-      $.get("./analyzer/api/patients", (result) => {
-          console.log(result);
+  componentWillMount = () => {
+    $.get("./api/patient", (result) => {
+      this.setState({
+        patients: result,
+        view: View.MAIN
       })
-    };
+    });
+  };
 
-    render() {
-        return (
-          <select>
-              <option value="volvo">Volvo</option>
-              <option value="saab">Saab</option>
-              <option value="vw">VW</option>
-              <option value="audi" selected>Audi</option>
-          </select>
-        );
+  private onClick = (id: number) => {
+    this.setState({
+      patients: this.state.patients,
+      currentPatientId: id,
+      view: View.PATIENT
+    });
+    console.log("OnClick", id);
+  };
+
+  createPatients = () => {
+    return this.state && this.state.patients.map((patient: any) => {
+      return (
+        <div id={patient.id} onClick={this.onClick.bind(this, patient.id)}>
+          {patient.id + ". " + patient.first_name + " " + patient.last_name}
+        </div>
+      )
+    });
+  };
+
+  private renderChild = () => {
+    if(this.state.view === View.MAIN) {
+      return (
+        <div>
+          {this.createPatients()}
+        </div>
+      );
+    } else if (this.state.view === View.PATIENT) {
+      return (
+        <PatientCharts id={this.state.currentPatientId}/>
+      );
     }
+    return null;
+  };
+
+  render() {
+
+    return (
+      <div>
+        {this.renderChild()}
+      </div>
+    );
+  }
 }
